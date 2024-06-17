b80974142cad8de390c40a437822a9696d1c5eb6
scott <scottysora@gmail.com>
2024-06-17 13:03:37 -0400

    define employee class

---
+++ exercises/classes.js
@@ -0,0 +1,25 @@
+class Employee {
+  constructor(id, firstName, lastName, jobTitle, payRate) {
+    this.employeeId = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.jobTitle = jobTitle;
+    this.payRate = payRate;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.5);
+console.log(`Employee ${employee1.fullName} created`);
+console.log(`Job title is ${employee1.jobTitle}`);
+console.log(`Pay rate is $${employee1.payRate}`);
+
+console.log();
+
+let employee2 = new Employee(2, "Sarah", "Lee", "Software Engineer", 50.0);
+console.log(`Employee ${employee2.fullName} created`);
+console.log(`Job title is ${employee2.jobTitle}`);
+console.log(`Pay rate is $${employee2.payRate}`);
